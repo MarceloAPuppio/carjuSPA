93828c1be25c7202ad4caaec7f37e57eff439f5d
marceloapuppio <marcelopuppio@gmail.com>
2021-10-28 23:59:47 -0300

    logo svg

--- app/components/sections/Team.js
+++ app/components/sections/Team.js
@@ -5,7 +5,7 @@ export const Team = () => {
   <div class="equipoCardsContainer">
       <div class="card">
           <div class="cardHeader">
-              <img class="card-image" src="https://source.unsplash.com/random/100x100?123" alt="">
+              <img class="card-image" src="https://source.unsplash.com/random/500x500?123" alt="">
           </div>
           <div class="cardDescription">
               <h4>Nombre</h4>
@@ -22,7 +22,7 @@ export const Team = () => {
       </div>
       <div class="card">
           <div class="cardHeader">
-              <img class="card-image" src="https://source.unsplash.com/random/100x100?3" alt="">
+              <img class="card-image" src="https://source.unsplash.com/random/500x500?3" alt="">
           </div>
           <div class="cardDescription">
               <h4>Nombre</h4>
@@ -39,7 +39,7 @@ export const Team = () => {
       </div>
       <div class="card">
           <div class="cardHeader">
-              <img class="card-image" src="https://source.unsplash.com/random/100x100?13" alt="">
+              <img class="card-image" src="https://source.unsplash.com/random/500x500?13" alt="">
           </div>
           <div class="cardDescription">
               <h4>Nombre</h4>
@@ -55,6 +55,23 @@ export const Team = () => {
           </div>
       </div>
 
+      <div class="card">
+      <div class="cardHeader">
+          <img class="card-image" src="https://source.unsplash.com/random/500x500?1113" alt="">
+      </div>
+      <div class="cardDescription">
+          <h4>Nombre</h4>
+          <h6>Cargo</h6>
+      </div>
+      <div class="cardFooter">
+          <div class="socialmediacontainer">
+              <i class="fab fa-linkedin-in"></i>
+          </div>
+          <div class="socialmediacontainer">
+              <i class="fas fa-envelope-open"></i>
+          </div>
+      </div>
+  </div>
 
 </section>
     `;
