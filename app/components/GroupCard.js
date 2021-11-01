e8e300c4b0acc153518ee578c3bec4a36389b1e2
marceloapuppio <marcelopuppio@gmail.com>
2021-11-01 01:35:06 -0300

    Ajustes estilos

--- app/components/GroupCard.js
+++ app/components/GroupCard.js
@@ -3,7 +3,12 @@ import { ProfileCard } from "./ProfileCard.js";
 export const GroupCard = (group) => {
   return `
 <details class="detail">
-  <summary>${group.nombre}</summary>
+  <summary><div class="detail-name">${
+    group.nombre
+  }</div><div class="thumbnail-wrapper">${group.personal
+    .map((empleado) => `<img class="thumbnail" src="${empleado.img}"/>`)
+    .join("")}
+</div></summary>
   <div class="equipoCardsContainer">
     ${group.personal.map((x) => ProfileCard(x)).join("")}
   </div>
