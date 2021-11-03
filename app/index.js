2c8f6a29aa9c88c83dc00da289a50022a09065e3
marceloapuppio <marcelopuppio@gmail.com>
2021-11-03 00:38:57 -0300

    API Intersection Observer

--- app/index.js
+++ app/index.js
@@ -1,2 +1,42 @@
 import { APP } from "./App.js";
+
+const cargarContenido = (entradas, observador) => {
+  entradas.forEach((entrada) => {
+    if (entrada.isIntersecting) {
+      console.log(entrada.target);
+      entrada.target.classList.add("visible");
+    } else {
+    }
+  });
+};
+const cargarImagen = (entradas, observador) => {
+  entradas.forEach((entrada) => {
+    if (entrada.isIntersecting) {
+      console.log(entrada.target);
+      entrada.target.classList.add("clipPath");
+    } else {
+      entrada.target.classList.remove("clipPath");
+    }
+  });
+};
+const observer = new IntersectionObserver(cargarContenido, {
+  root: null,
+  rootMargin: "200px",
+  threshold: 0.9,
+});
+const observerImg = new IntersectionObserver(cargarImagen, {
+  root: null,
+  rootMargin: "200px",
+  threshold: 0.9,
+});
 document.addEventListener("DOMContentLoaded", APP);
+window.addEventListener("load", () => {
+  const clipPath = document.querySelector(".about-img");
+  const details = document.querySelectorAll("details");
+  console.log(details);
+
+  details.forEach((e) => {
+    observer.observe(e);
+  });
+  observerImg.observe(clipPath);
+});
