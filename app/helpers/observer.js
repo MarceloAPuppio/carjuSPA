06c545769a067fbd8c9caa1510a73527a2caf609
marceloapuppio <marcelopuppio@gmail.com>
2021-11-03 00:41:58 -0300

    refactor observer.js

---
+++ app/helpers/observer.js
@@ -0,0 +1,27 @@
+const cargarContenido = (entradas, observador) => {
+  entradas.forEach((entrada) => {
+    if (entrada.isIntersecting) {
+      entrada.target.classList.add("visible");
+    } else {
+    }
+  });
+};
+const cargarImagen = (entradas, observador) => {
+  entradas.forEach((entrada) => {
+    if (entrada.isIntersecting) {
+      entrada.target.classList.add("clipPath");
+    } else {
+      entrada.target.classList.remove("clipPath");
+    }
+  });
+};
+export const observer = new IntersectionObserver(cargarContenido, {
+  root: null,
+  rootMargin: "200px",
+  threshold: 0.9,
+});
+export const observerImg = new IntersectionObserver(cargarImagen, {
+  root: null,
+  rootMargin: "200px",
+  threshold: 0.9,
+});
--- app/index.js
+++ app/index.js
@@ -1,34 +1,6 @@
 import { APP } from "./App.js";
+import { observer, observerImg } from "./helpers/observer.js";
 
-const cargarContenido = (entradas, observador) => {
-  entradas.forEach((entrada) => {
-    if (entrada.isIntersecting) {
-      console.log(entrada.target);
-      entrada.target.classList.add("visible");
-    } else {
-    }
-  });
-};
-const cargarImagen = (entradas, observador) => {
-  entradas.forEach((entrada) => {
-    if (entrada.isIntersecting) {
-      console.log(entrada.target);
-      entrada.target.classList.add("clipPath");
-    } else {
-      entrada.target.classList.remove("clipPath");
-    }
-  });
-};
-const observer = new IntersectionObserver(cargarContenido, {
-  root: null,
-  rootMargin: "200px",
-  threshold: 0.9,
-});
-const observerImg = new IntersectionObserver(cargarImagen, {
-  root: null,
-  rootMargin: "200px",
-  threshold: 0.9,
-});
 document.addEventListener("DOMContentLoaded", APP);
 window.addEventListener("load", () => {
   const clipPath = document.querySelector(".about-img");
